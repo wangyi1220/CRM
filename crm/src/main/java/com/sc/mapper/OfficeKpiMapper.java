1193d10f27a1ec37a6b15b36bb82b4ae7c67cc11
wangyi1220 <3220433298@qq.com>
2019-12-10 21:32:21 +0800

    提交新代码

--- crm/src/main/java/com/sc/mapper/OfficeKpiMapper.java
+++ crm/src/main/java/com/sc/mapper/OfficeKpiMapper.java
@@ -2,6 +2,7 @@ package com.sc.mapper;
 
 import com.sc.entity.OfficeKpi;
 import com.sc.entity.OfficeKpiExample;
+import java.math.BigDecimal;
 import java.util.List;
 import org.apache.ibatis.annotations.Param;
 
@@ -10,13 +11,21 @@ public interface OfficeKpiMapper {
 
     int deleteByExample(OfficeKpiExample example);
 
+    int deleteByPrimaryKey(BigDecimal kpiId);
+
     int insert(OfficeKpi record);
 
     int insertSelective(OfficeKpi record);
 
     List<OfficeKpi> selectByExample(OfficeKpiExample example);
 
+    OfficeKpi selectByPrimaryKey(BigDecimal kpiId);
+
     int updateByExampleSelective(@Param("record") OfficeKpi record, @Param("example") OfficeKpiExample example);
 
     int updateByExample(@Param("record") OfficeKpi record, @Param("example") OfficeKpiExample example);
+
+    int updateByPrimaryKeySelective(OfficeKpi record);
+
+    int updateByPrimaryKey(OfficeKpi record);
 }
