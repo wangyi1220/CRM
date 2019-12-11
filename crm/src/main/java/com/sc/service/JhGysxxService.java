a250c5df63f01fc69544353a2a31e9abb8b15790
1307566754 <1307566754@qq.com>
2019-12-11 21:31:48 +0800

    付宏伟12.11

---
+++ crm/src/main/java/com/sc/service/JhGysxxService.java
@@ -0,0 +1,24 @@
+//进货（供应商信息）
+package com.sc.service;
+
+import java.util.List;
+
+import com.github.pagehelper.PageInfo;
+import com.sc.entity.JhGysxx;
+
+
+public interface JhGysxxService {
+		//查询
+		public List<JhGysxx> list();
+		//添加
+		public void add(JhGysxx gys);
+		//修改
+		public void update(JhGysxx gys);
+		//删除
+		public void delete(String gysId);
+		//获取对象
+		public JhGysxx get(String gysId);
+		//分页查询
+		public PageInfo<JhGysxx> selectpage(Integer pageNum,Integer pageSize);
+
+}
