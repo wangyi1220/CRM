d946b7c802f8a0295602c22e98ea7bded8e9eb91
wangyi1220 <3220433298@qq.com>
2019-12-12 16:21:50 +0800

    日期注解和tostring--12.12

--- crm/src/main/java/com/sc/entity/KcGoods.java
+++ crm/src/main/java/com/sc/entity/KcGoods.java
@@ -3,6 +3,8 @@ package com.sc.entity;
 import java.io.Serializable;
 import java.util.Date;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 public class KcGoods implements Serializable {
     private Long goodsId;
 
@@ -28,9 +30,18 @@ public class KcGoods implements Serializable {
 
     private Long companyId;
 
+    @DateTimeFormat(pattern="yyyy-MM-dd HH:mm:ss")
     private Date lastModifyTime;
 
-    private static final long serialVersionUID = 1L;
+    @Override
+	public String toString() {
+		return "KcGoods [goodsId=" + goodsId + ", goodsName=" + goodsName + ", goodsKinds=" + goodsKinds
+				+ ", goodsSpecs=" + goodsSpecs + ", unit=" + unit + ", cangkuId=" + cangkuId + ", kcNum=" + kcNum
+				+ ", goodsCost=" + goodsCost + ", goodsPrice=" + goodsPrice + ", goodsSellingPrice=" + goodsSellingPrice
+				+ ", note=" + note + ", companyId=" + companyId + ", lastModifyTime=" + lastModifyTime + "]";
+	}
+
+	private static final long serialVersionUID = 1L;
 
     public KcGoods(Long goodsId, String goodsName, String goodsKinds, String goodsSpecs, String unit, Long cangkuId, String kcNum, String goodsCost, String goodsPrice, String goodsSellingPrice, String note, Long companyId, Date lastModifyTime) {
         this.goodsId = goodsId;
