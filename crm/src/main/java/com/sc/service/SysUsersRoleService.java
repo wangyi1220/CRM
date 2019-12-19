3265f990783e21311d576f9aad306cd015511351
wangyi1220 <3220433298@qq.com>
2019-12-19 18:56:25 +0800

    余金少

--- crm/src/main/java/com/sc/service/SysUsersRoleService.java
+++ crm/src/main/java/com/sc/service/SysUsersRoleService.java
@@ -5,4 +5,5 @@ import com.sc.entity.SysUsersRole;
 public interface SysUsersRoleService {
 	public void insert(SysUsersRole sur);
 	public void update(SysUsersRole sur);
+	public SysUsersRole selectByUId(Long uId);
 }
